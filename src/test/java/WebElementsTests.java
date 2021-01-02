ad17445f646ba06ac8c5be8e2957c1f9a46bf1fc
ilusi0n <mateuszciolek@gmail.com>
2021-01-02 08:45:13 +0100

    Created typingIntoWebElementTest

--- src/test/java/WebElementsTests.java
+++ src/test/java/WebElementsTests.java
@@ -1,7 +1,12 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class WebElementsTests {
 
@@ -12,6 +17,25 @@ public class WebElementsTests {
         System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.navigate().to("http://przyklady.javastart.pl/test/full_form.html");
+    }
+
+    @Test
+    public void typingIntoWebElementTest() {
+
+        WebElement userNameField = driver.findElement(By.id("username"));
+        sleep();
+        userNameField.sendKeys("Selenium Start");
+        String typeUserNameValue = userNameField.getAttribute("value");
+        sleep();
+        assertEquals(typeUserNameValue, "Selenium Start");
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterMethod
