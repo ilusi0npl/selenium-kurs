95bbb54d1953b34077193853f7c3cb6482e9ede1
ilusi0n <mateuszciolek@gmail.com>
2021-01-02 09:42:10 +0100

    Added SecondAutomatedTest.java

---
+++ src/test/java/SecondAutomatedTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SecondAutomatedTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void mySecondTest() {
+        driver.navigate().to("http://selenium.dev/");
+
+        String pageTitle = driver.getTitle();
+
+        assertTrue(pageTitle.equals("SeleniumHQ Browser Automation"));
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+
+}
