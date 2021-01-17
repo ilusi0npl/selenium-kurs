17d435f00e4c6c798a841b869fd0138ef9cd7eaa
ilusi0n <mateuszciolek@gmail.com>
2021-01-17 14:57:07 +0100

    Added IFrameTests.java

---
+++ src/test/java/IFrameTests.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class IFrameTests {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void iFrameTest() {
+        driver.navigate().to("http://theinernet.przyklady.javastart.pl/nested_frames");
+
+        //Przejście do ramki nadrzędnej
+        driver.switchTo().frame("frame-top");
+
+        //Przejście do ramki podrzędnej - właściwej z tekstem
+        driver.switchTo().frame("frame-left");
+
+        By bodyLocator = By.xpath("//body");
+        String leftFrameText = driver.findElement(bodyLocator).getText();
+        assertEquals(leftFrameText, "LEFT");
+
+        //Powrót to głównej ramki
+        driver.switchTo().defaultContent();
+
+        //Przejście do ramki o nazwie bottom
+        driver.switchTo().frame("frame-bottom");
+
+        String bottomFrameText = driver.findElement(bodyLocator).getText();
+        assertEquals(bottomFrameText, "BOTTOM");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+
+}
