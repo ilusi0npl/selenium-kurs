0af7be7c5bbc9188059c7397b8207f2964c41384
ilusi0n <mateuszciolek@gmail.com>
2022-11-25 17:50:54 +0100

    Import fix

--- src/test/java/ExplicitWaitTests.java
+++ src/test/java/ExplicitWaitTests.java
@@ -8,6 +8,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 import static org.testng.Assert.*;
 
 public class ExplicitWaitTests {
