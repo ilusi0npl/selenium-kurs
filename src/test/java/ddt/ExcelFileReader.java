18a21099d0f5342f462c69fc03495f5d2e20648f
mdel2021 <79173434+mdel2021@users.noreply.github.com>
2024-02-07 23:12:30 +0100

    Update ExcelFileReader.java
    sheetName updated to DuckDuckGoSearch

--- src/test/java/ddt/ExcelFileReader.java
+++ src/test/java/ddt/ExcelFileReader.java
@@ -7,24 +7,24 @@ public class ExcelFileReader {
 
     public static void main(String[] args) {
 
-        //Indeks określający kolumnę o nazwie Search Query w arkuszu GoogleSearch
+        //Indeks określający kolumnę o nazwie Search Query w arkuszu DuckDuckGoSearch
         int queryColumn = 0;
 
-        //Indeks określający kolumnę o nazwie Expected result w arkuszu GoogleSearch
+        //Indeks określający kolumnę o nazwie Expected result w arkuszu DuckDuckGoSearch
         int expectedResultColumn = 1;
 
         //Utworzenie instancji klasy ExcelSheetReader
         ExcelSheetReader excelSheetReader = new ExcelSheetReader();
 
         try {
-            //Odczytanie/Pobranie pliku o nazwie DDT_FILE.xslx oraz arkusza wewnątrz pliku o nazwie GoogleSearch
+            //Odczytanie/Pobranie pliku o nazwie DDT_FILE.xslx oraz arkusza wewnątrz pliku o nazwie DuckDuckGoSearch
             String excelFileLocation = System.getProperty("user.dir") + "/src/main/resources/" + "DDT_FILE.xlsx";
-            excelSheetReader.setExcelFileSheet(excelFileLocation, "GoogleSearch");
+            excelSheetReader.setExcelFileSheet(excelFileLocation, "DuckDuckGoSearch");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //Utworzenie instancji klasy SheetDataReader, która w konstruktorze przyjmuje obiekt arkusza - w naszym wypadku jest to arkusz o nazwie Google Search
+        //Utworzenie instancji klasy SheetDataReader, która w konstruktorze przyjmuje obiekt arkusza - w naszym wypadku jest to arkusz o nazwie DuckDuckGoSearch
         SheetDataReader sheetDataReader = new SheetDataReader(excelSheetReader.getExcelSheet());
 
         //Pobranie wartości dla kolumny Search query
